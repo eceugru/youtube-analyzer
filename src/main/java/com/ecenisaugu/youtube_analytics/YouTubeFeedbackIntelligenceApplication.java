df2e810ec4634ce97d5750ae25a18c3762d4e3e8
Ece Nisa Ugur <ecenisaugu@gmail.com>
2025-10-12 12:25:33 +0300

    Initial commit: project setup

---
+++ src/main/java/com/ecenisaugu/youtube_analytics/YouTubeFeedbackIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.ecenisaugu.youtube_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeFeedbackIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeFeedbackIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecenisaugu/youtube_analytics/YouTubeFeedbackIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecenisaugu.youtube_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTubeFeedbackIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
